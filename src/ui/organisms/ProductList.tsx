905e65fb986873440bc6334bcd061651f0d2f443
adam <adam.jacewicz4@gmail.com>
2023-09-14 21:46:35 +0200

    - added data-testid attr to product list

--- src/ui/organisms/ProductList.tsx
+++ src/ui/organisms/ProductList.tsx
@@ -7,7 +7,10 @@ type ProductListProps = {
 
 export const ProductList = ({ products }: ProductListProps) => {
 	return (
-		<ul className="gap grid grid-cols-1 gap-8 sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+		<ul
+			data-testid="products-list"
+			className="gap grid grid-cols-1 gap-8 sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+		>
 			{products.map((product) => (
 				<ProductListItem key={product.id} product={product} />
 			))}
