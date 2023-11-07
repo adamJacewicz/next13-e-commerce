79376eb379e8f9e97550e92ebb8e3b138caec3d8
adam <adam.jacewicz4@gmail.com>
2023-11-07 16:23:22 +0100

    - task4

--- src/components/atoms/AddToCartButton.tsx
+++ src/components/atoms/AddToCartButton.tsx
@@ -1,16 +1,11 @@
 "use client";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import { useFormStatus } from "react-dom";
+// import { useFormStatus } from "react-dom";
 
 export function AddToCartButton() {
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-assignment
-	const formStatus = useFormStatus();
+	// const formStatus = useFormStatus();
 
 	return (
 		<button
-			{/* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access */}
-			disabled={formStatus.pending}
 			type="submit"
 			data-testid="add-to-cart-button"
 			className="w-full rounded-md border border-transparent bg-blue-600 py-2 font-medium text-white hover:bg-blue-700 disabled:bg-blue-300"
